e3fabdb5ed7faf1688445ab0aa6eb65ffbc7370f
Tolu <olabiyitolu@gmail.com>
2023-11-30 23:25:12 +0100

    import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,4 @@
-#usr/bin/python3
+#!/usr/bin/python3
 if __name__ = "__main__":
     "print number of arguments"
     import sys
@@ -12,4 +12,3 @@ if __name__ = "__main__":
         print("{} arguments".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-        
