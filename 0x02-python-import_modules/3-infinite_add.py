de6c64d75fbf0409684c40ae15f40dba3c7008d6
Tolu <olabiyitolu@gmail.com>
2023-12-04 13:02:03 +0100

    update

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+if __name__ = "__main__":
+    from calculator_1 import add
+    import sys
+
+    count = len(sys.argv)
+    sum = 0
+    for i in range(count - 1):
+        sum += int(sys.argv[i] + 1)
+    print(f'{}'.format(sum))
