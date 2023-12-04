5613b2d1601d0e4606f9244738ef95d27566923a
Tolu <olabiyitolu@gmail.com>
2023-12-04 13:18:29 +0100

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
     import sys
 
     count = len(sys.argv)
     sum = 0
     for i in range(count - 1):
-        sum += int(i + 1)
+        sum += int(sys.argv[i] + 1)
     print('{}'.format(sum))
