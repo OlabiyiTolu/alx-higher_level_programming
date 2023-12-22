95cb6e48324b7322485a0f1267a960964ee7ce5c
tolu <olabiyitolu@gmail.com>
2023-12-22 12:48:06 +0100

    mult

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentences):
     mult = ()
     if len(sentences) == 0:
-        mult = 0, "None" 
+        mult = 0, "None"
     else:
         mult = len(sentences), sentences[0]
     return mult
