148c6532a5ee94b793e3313f65dbc1de1a744f97
Tolu <olabiyitolu@gmail.com>
2024-03-16 05:39:43 -0700

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log(`My number is ${num}`);
+  console.log(`My number: ${num}`);
 }
